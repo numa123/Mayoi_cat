d390f7ec72d2b86f4e65a0e0650bc2f8f02aff49
numa123 <taka0201ttt@gmail.com>
2022-12-26 16:59:26 +0900

    change barrier color

--- lib/main.dart
+++ lib/main.dart
@@ -45,175 +45,170 @@ class _MyHomePageState extends State<MyHomePage> {
           bottom: BorderSide(color: Colors.grey, width: 1),
         ),
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            const Padding(padding: EdgeInsets.all(10)),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: '1つ目',
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 1,
-                      color: Colors.grey,
-                    ),
+      body: Column(
+        children: <Widget>[
+          const Padding(padding: EdgeInsets.all(10)),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: '1つ目',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 1,
+                    color: Colors.grey,
                   ),
                 ),
-                maxLength: 20,
-                onChanged: (text) {
-                  setState(() {
-                    inputText = text;
-                  });
-                },
               ),
+              maxLength: 20,
+              onChanged: (text) {
+                setState(() {
+                  inputText = text;
+                });
+              },
             ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 10,
-                right: 10,
-              ),
-              child: TextField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: '2つ目',
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 1,
-                      color: Colors.grey,
-                    ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 10,
+              right: 10,
+            ),
+            child: TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: '2つ目',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 1,
+                    color: Colors.grey,
                   ),
                 ),
-                maxLength: 20,
-                onChanged: (text) {
-                  setState(() {
-                    inputText2 = text;
-                  });
-                },
               ),
+              maxLength: 20,
+              onChanged: (text) {
+                setState(() {
+                  inputText2 = text;
+                });
+              },
             ),
-            Padding(padding: EdgeInsets.only(top: 10)),
-            TextButton(
-              child: const Text(
-                '決めていただく',
-                style: TextStyle(color: Colors.blueAccent, fontSize: 18),
-              ),
-              onPressed: () async {
-                var random = math.Random();
-                var isOK = random.nextBool();
-                var resultValue;
-                if (isOK == true) {
-                  resultValue = inputText;
-                } else {
-                  resultValue = inputText2;
-                }
-                // ダイアログを表示------------------------------------
-                var result = await showDialog<int>(
-                  context: context,
-                  barrierDismissible: false,
-                  builder: (BuildContext context) {
-                    if (inputText == '' || inputText2 == '') {
-                      return AlertDialog(
-                        content: Container(
-                          height: 150,
+          ),
+          const Padding(padding: EdgeInsets.only(top: 10)),
+          TextButton(
+            child: const Text(
+              '決めていただく',
+              style: TextStyle(color: Colors.blueAccent, fontSize: 18),
+            ),
+            onPressed: () async {
+              var random = math.Random();
+              var isOK = random.nextBool();
+              String resultValue;
+              if (isOK == true) {
+                resultValue = inputText;
+              } else {
+                resultValue = inputText2;
+              }
+              // ダイアログを表示------------------------------------
+              var result = await showDialog<int>(
+                barrierColor: Colors.orange.shade100.withOpacity(0.2),
+                context: context,
+                barrierDismissible: false,
+                builder: (BuildContext context) {
+                  if (inputText == '' || inputText2 == '') {
+                    return AlertDialog(
+                      elevation: 2,
+                      content: SizedBox(
+                        height: 150,
+                        child: Column(
+                          children: [
+                            const Text('迷っていることを入力してね'),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 40),
+                              child: SizedBox(
+                                width: 85,
+                                height: 85,
+                                child: Image.asset(
+                                  'images/cool.png',
+                                  fit: BoxFit.contain,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      actions: <Widget>[
+                        TextButton(
+                          child: const Text(
+                            'OK',
+                            style: TextStyle(color: Colors.blueAccent),
+                          ),
+                          onPressed: () => Navigator.of(context).pop(1),
+                        ),
+                      ],
+                    );
+                    ;
+                  } else {
+                    return AlertDialog(
+                      elevation: 2,
+                      title: const Text(
+                        '選んでいただいたものは',
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      content: Padding(
+                        padding: const EdgeInsets.only(top: 6.0),
+                        child: SizedBox(
+                          height: 170,
                           child: Column(
                             children: [
-                              Text('迷っていることを入力してね'),
+                              Text(
+                                resultValue,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.normal),
+                              ),
                               Padding(
-                                padding: EdgeInsets.only(top: 40),
+                                padding: const EdgeInsets.only(top: 40),
                                 child: SizedBox(
-                                  width: 85,
-                                  height: 85,
+                                  width: 100,
+                                  height: 100,
                                   child: Image.asset(
                                     'images/cool.png',
                                     fit: BoxFit.contain,
                                   ),
                                 ),
-                              )
+                              ),
                             ],
                           ),
                         ),
-                        actions: <Widget>[
-                          TextButton(
-                            child: const Text(
-                              'OK',
-                              style: TextStyle(color: Colors.blueAccent),
-                            ),
-                            onPressed: () => Navigator.of(context).pop(1),
+                      ),
+                      actions: <Widget>[
+                        TextButton(
+                          child: const Text(
+                            'OK',
+                            style: TextStyle(color: Colors.blueAccent),
                           ),
-                        ],
-                      );
-                      ;
-                    } else {
-                      return AlertDialog(
-                        title: const Text(
-                          '選んでいただいたものは',
-                          style: TextStyle(fontSize: 16),
+                          onPressed: () => Navigator.of(context).pop(1),
                         ),
-                        content: Padding(
-                          padding: const EdgeInsets.only(top: 6.0),
-                          child: Container(
-                            height: 170,
-                            child: Column(
-                              children: [
-                                Container(
-                                  child: Text(
-                                    '$resultValue',
-                                    textAlign: TextAlign.center,
-                                    style: const TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.normal),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(top: 40),
-                                  child: Container(
-                                    width: 100,
-                                    height: 100,
-                                    child: Image.asset(
-                                      'images/cool.png',
-                                      fit: BoxFit.contain,
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                        actions: <Widget>[
-                          // TextButton(
-                          //   child: const Text('閉じる', style: TextStyle(color: Colors.blueAccent),),
-                          //   onPressed: () => Navigator.of(context).pop(0),
-                          // ),
-                          TextButton(
-                            child: const Text(
-                              'OK',
-                              style: TextStyle(color: Colors.blueAccent),
-                            ),
-                            onPressed: () => Navigator.of(context).pop(1),
-                          ),
-                        ],
-                      );
-                    }
-                  },
-                );
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: SizedBox(
-                width: 170,
-                height: 170,
-                child: Image.asset(
-                  'images/cool.png',
-                  fit: BoxFit.contain,
-                ),
+                      ],
+                    );
+                  }
+                },
+              );
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: SizedBox(
+              width: 170,
+              height: 170,
+              child: Image.asset(
+                'images/cool.png',
+                fit: BoxFit.contain,
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
